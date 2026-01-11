2676b635fc329ecee45a323b9101126b2a682f4a
azharanas17 <azharanas173@gmail.com>
2026-01-11 11:42:12 +0700

    add link to icon instagram and linkedin on contact section

--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -30,22 +30,26 @@ function ContactSection() {
               <span>{personalData.email}</span>
             </p>
             <p className="text-sm md:text-xl flex items-center gap-3">
-              <FaInstagram
-                className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#10b981] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
-                size={36}
-              />
-              <span>
-                {personalData.instagramUsername}
-              </span>
+              <Link target="_blank" href={personalData.instagram}>
+                <FaInstagram
+                  className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#10b981] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
+                  size={36}
+                />
+                <span>
+                  {personalData.instagramUsername}
+                </span>
+              </Link>
             </p>
             <p className="text-sm md:text-xl flex items-center gap-3">
-              <FaLinkedin
-                className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#10b981] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
-                size={36}
-              />
-              <span>
-                {personalData.linkedInUsername}
-              </span>
+              <Link target="_blank" href={personalData.linkedIn}>
+                <FaLinkedin
+                  className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#10b981] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
+                  size={36}
+                />
+                <span>
+                  {personalData.linkedInUsername}
+                </span>
+              </Link>
             </p>
           </div>
           {/* <div className="mt-8 lg:mt-16 flex items-center gap-5 lg:gap-10">
